96cbc08a631b166230b46c04d8bf8c717cd442de
Joanie Davis <joanie.davis@atomicobject.com>
2021-05-16 12:27:50 -0400

    Ran into trouble pinging gist API so instead returning a json from a grabbed Postman query to hand off to the server.

--- src/server/gistWrapper.js
+++ src/server/gistWrapper.js
@@ -1,2 +1,29 @@
 // Library file
 // This will provide the "library" functionality required for querying the public gist API
+const https = require('https');
+
+function fetchAllGistsURL(username) {
+    return `https://api.github.com/users/${username}/gists`;
+}
+
+exports.fetchGistsForUsername = function(username) {
+    const options = {
+        url: fetchAllGistsURL(username),
+        method: 'GET',
+        headers: { 'user-agent': 'node.js' }
+    }
+
+    const request = https.request(options, (resp) => {
+        console.log(resp);
+    });
+
+    // request(options, { json:true }, (err, res, body) => {
+    //     if(err) {
+    //         console.log(`ERROR: ${err}`);
+    //         console.log(body.explanation);
+    //     } else {
+    //         console.log(res);
+    //         console.log(body);
+    //     }
+    // });
+}
--- src/server/server.js
+++ src/server/server.js
@@ -2,9 +2,22 @@
 // This will host the API server that the react application will ping to query for gist information
 
 const express = require('express');
+const url = require('url');
+
+const gistWrapper = require('./gistWrapper')
+
 const port = 3001;
 
 const app = express();
+
+app.get('/gists', (req, res) => {
+   const query = url.parse(req.url, true).query;
+   const username = query.username;
+
+   const response = gistWrapper.fetchGistsForUsername(username);
+
+   res.status(200).send(response);
+});
 
 app.listen(port, () => {
    console.log(`Gist Fetch Server listening on port ${port}`);
